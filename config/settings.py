6af184b16b6796682389767f38b811b396acc88d
zizimoos_iMac <zizimoos@gmail.com>
2020-11-30 05:51:44 +0900

    USER APP done!

--- config/settings.py
+++ config/settings.py
@@ -28,9 +28,7 @@ DEBUG = True
 ALLOWED_HOSTS = []
 
 
-# Application definition
-
-INSTALLED_APPS = [
+DJANGO_APPS = [
     "django.contrib.admin",
     "django.contrib.auth",
     "django.contrib.contenttypes",
@@ -38,6 +36,12 @@ INSTALLED_APPS = [
     "django.contrib.messages",
     "django.contrib.staticfiles",
 ]
+
+# Application definition
+
+PROJECT_APPS = ["users.apps.UsersConfig"]
+
+INSTALLED_APPS = DJANGO_APPS + PROJECT_APPS
 
 MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
@@ -118,3 +122,5 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = "/static/"
+
+AUTH_USER_MODEL = "users.User"
--- users/models.py
+++ users/models.py
@@ -1,3 +1,34 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
-# Create your models here.
+
+class User(AbstractUser):
+    """ Custom User Model """
+
+    GENDER_MALE = "male"
+    GENDER_FEMALE = "female"
+    GENDER_OTHER = "other"
+
+    GENDER_CHOICES = (
+        (GENDER_MALE, "male"),
+        (GENDER_FEMALE, "female"),
+        (GENDER_OTHER, "other"),
+    )
+
+    LANGUAGE_ENGLISH = "english"
+    LANGUAGE_KOREAN = "korean"
+
+    LANGUAGE_CHOICE = ((LANGUAGE_ENGLISH, "english"), (LANGUAGE_KOREAN, "korean"))
+
+    CURRENCY_USD = "USD"
+    CURRENCY_KRW = "KRW"
+
+    CURRENCY_CHOICE = ((CURRENCY_USD, "USD"), (CURRENCY_KRW, "KRW"))
+
+    avatar = models.ImageField(blank=True)
+    gender = models.CharField(choices=GENDER_CHOICES, max_length=10, blank=True)
+    bio = models.TextField(blank=True)
+    birthdate = models.DateField(blank=True, null=True)
+    language = models.CharField(choices=LANGUAGE_CHOICE, max_length=10, blank=True)
+    currency = models.CharField(choices=CURRENCY_CHOICE, max_length=5, blank=True)
+    superhost = models.BooleanField(default=False)
