2bca7fd51d78835b7ad7602ee72aa7f0ee37001e
zizimoos_iMac <zizimoos@gmail.com>
2020-12-05 08:45:50 +0900

    ROOM APP done!

--- rooms/admin.py
+++ rooms/admin.py
@@ -2,13 +2,23 @@ from django.contrib import admin
 from . import models
 
 
-@admin.register(models.RoomType)
+@admin.register(models.RoomType, models.Facility, models.Amenity, models.HouseRule)
 class ItemAdmin(admin.ModelAdmin):
+    """Item Admin Definition"""
+
     pass
 
 
 # Register your models here.
 @admin.register(models.Room)
 class RoomAdmin(admin.ModelAdmin):
+    """Room Admin Definition"""
+
+    pass
+
+
+@admin.register(models.Photo)
+class PhotoAdmin(admin.ModelAdmin):
+    """ """
 
     pass
