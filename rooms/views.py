4a3886abe46d0f390ff519913d0813f28c6fbdf6
zizimoos_iMac <zizimoos@gmail.com>
2020-12-09 00:18:47 +0900

    Pages List Navigation done!

--- rooms/views.py
+++ rooms/views.py
@@ -1,3 +1,4 @@
+from math import ceil
 from django.shortcuts import render
 from . import models
 
@@ -5,5 +6,20 @@ from . import models
 
 
 def all_rooms(request):
-    all_rooms = models.Room.objects.all()
-    return render(request, "rooms/home.html", context={"potato": all_rooms})
+    page = request.GET.get("page", 1)
+    page = int(page or 1)
+    page_size = 10
+    limit = page_size * page
+    offset = limit - page_size
+    page_count = models.Room.objects.count() / ceil(page_size)
+    all_rooms = models.Room.objects.all()[offset:limit]
+    return render(
+        request,
+        "rooms/home.html",
+        context={
+            "potato": all_rooms,
+            "page": page,
+            "page_count": int(page_count),
+            "page_range": range(1, int(page_count) + 1),
+        },
+    )
